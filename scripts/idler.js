7ef629154406387f3dfc7048bf769d7b15167202
Ryuutan <OleKrK@hotmail.com>
2022-12-21 03:25:11 +1100

    Full-stack realization.
    Co-authored-by: HexcodeC000 <HexcodeC000@users.noreply.github.com>

--- scripts/idler.js
+++ scripts/idler.js
@@ -1,10 +1,67 @@
-// Here be idlers! 
-var storage = 0;
+/* 
+Storage - Capacity in bits
+RAM - More Traps
+CPU - More Entities
+GPU - Better vision? (Vitamin A not needed)
+Tier 7 data
+*/
 
-function displayResource(displayId) {
+
+/*
+
+fs = require('fs');
+var name = 'fileName.json';
+var m = JSON.parse(fs.readFileSync(name).toString());
+m.forEach(function(p){
+    p.name= m.name;
+});
+fs.writeFileSync(name, JSON.stringify(m));
+
+*/
+
+
+var resources = 
+[
+    {
+        name: 'storage',
+        amount: 0
+    },
+    {
+        name: 'ram',
+        amount: 0
+    },
+    {
+        name: 'cpu',
+        amount: 0
+    },
+    {
+        name: 'gpu',
+        amount: 0
+    },
+]
+
+// Here be idlers! 
+async function load() 
+{
+    const {default: data} = await import('../json/savedata.json', {
+        assert:{
+            type:"json"
+        }
+    });
+    console.log(data);
+}
+
+function save() 
+{
+    JSON.stringify(resources);
+}
+
+function displayResource(displayId) 
+{
 
 }
 
-function addResource(resourceId) {
+function addResource(resourceId) 
+{
     
 }
