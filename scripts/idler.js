214984a2c9ab70b4900144bd4915ee03b422b788
Ryuutan <OleKrK@hotmail.com>
2022-12-28 00:24:18 +1100

    Merge fixing bullshit
    We had a collective panic attack.

--- scripts/idler.js
+++ scripts/idler.js
@@ -4,11 +4,15 @@ RAM - More Traps
 CPU - More Entities
 GPU - Better vision? (Vitamin A not needed)
 Tier 7 data
+
+    - Append new resources to the resources variable
+    - Make resources locked and unlock them as soon as it is possible to gain them
+
+
 */
 
 
 /*
-
 fs = require('fs');
 var name = 'fileName.json';
 var m = JSON.parse(fs.readFileSync(name).toString());
@@ -19,32 +23,43 @@ fs.writeFileSync(name, JSON.stringify(m));
 
 */
 
+var clickValue = 1;
 
 var resources = 
 [
     {
-        name: 'storage',
-        amount: 0
+        name: 'Storage',
+        amount: 0,
+        autoValue: 0,
+        clickValue: 1
     },
     {
-        name: 'ram',
-        amount: 0
+        name: 'RAM',
+        amount: 0,
+        autoValue: 0,
+        clickValue: 1
     },
     {
-        name: 'cpu',
-        amount: 0
+        name: 'CPU',
+        amount: 0,
+        autoValue: 0,
+        clickValue: 1
     },
     {
-        name: 'gpu',
-        amount: 0
+        name: 'GPU',
+        amount: 0,
+        autoValue: 0,
+        clickValue: 1
     },
 ]
 
 // Here be idlers! 
 async function load() 
 {
-    const {default: data} = await import('../json/savedata.json', {
-        assert:{
+    const {default: data} = await import('../json/savedata.json', 
+    {
+        assert:
+        {
             type:"json"
         }
     });
@@ -56,12 +71,21 @@ function save()
     JSON.stringify(resources);
 }
 
-function displayResource(displayId) 
+function displayResource(displayId)
 {
+    var display = document.getElementById(displayId);
+    var idValue = displayId.substring(0, displayId.length - 4);
+    var res = resources[idValue];
+    
+    display.innerText = res.name + ": " + res.amount + ' bytes';
 
 }
 
-function addResource(resourceId) 
+function addResource(idValue) 
 {
-    
+    resources[idValue].amount += resources[idValue].clickValue;
+    resources[idValue].amount = Number(resources[idValue].amount.toFixed(3))
+    //Number(num2.toFixed(3))
+     
+    displayResource(idValue + "Disp");
 }
